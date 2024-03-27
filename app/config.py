e435451c0cd158dc24db9b7bfa6d36225d7e0053
HyScript7 <hyscript7@seznam.cz>
2024-03-27 15:26:35 +0100

    Add a config loader

---
+++ app/config.py
@@ -0,0 +1,15 @@
+import logging
+
+from decouple import config
+
+logger = logging.getLogger("strongest.config")
+
+TOKEN: str = config("BOT_TOKEN", None)
+if TOKEN is None:
+    logger.critical("BOT_TOKEN not found in .env file")
+    raise Exception("BOT_TOKEN not found in .env file")
+
+PREFIX: str = config("BOT_PREFIX", None)
+if PREFIX is None:
+    PREFIX = "&"
+    logger.warn("BOT_PREFIX not found in .env file, using default: '&'")
