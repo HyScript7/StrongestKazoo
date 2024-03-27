30018aa8240340ee4d720e60ea40f0e5fd498f3c
HyScript7 <hyscript7@seznam.cz>
2024-03-27 14:45:45 +0100

    Add logger configuration

---
+++ app/loggers.py
@@ -0,0 +1,41 @@
+import sys
+import logging
+from typing import List
+
+from colorlog import ColoredFormatter
+
+log_file = "latest.log"
+
+formatter = ColoredFormatter(
+        "%(asctime)s %(log_color)s%(levelname)-8s%(reset)s [%(name)s] %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+    reset=True,
+    log_colors={
+        "DEBUG": "cyan",
+        "INFO": "green",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "red,bg_white",
+    },
+    secondary_log_colors={},
+    style="%",
+)
+
+file_handler = logging.FileHandler(log_file)
+console_handler = logging.StreamHandler(sys.stdout)
+
+file_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+)
+console_handler.setFormatter(formatter)
+
+
+def configure_logger(loggers: List[str] = None): # type: ignore
+    if loggers is None:
+        loggers = []
+    for logger_name in loggers:
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.INFO)
+
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
