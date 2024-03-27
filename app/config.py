17d407b97368bf6f35929ca8ad1ea718df675741
HyScript7 <hyscript7@seznam.cz>
2024-03-27 21:03:21 +0100

    Add bot cache dir config key

--- app/config.py
+++ app/config.py
@@ -13,3 +13,9 @@ PREFIX: str = config("BOT_PREFIX", None)
 if PREFIX is None:
     PREFIX = "&"
     logger.warn("BOT_PREFIX not found in .env file, using default: '&'")
+
+
+CACHE_DIR: str = config("BOT_CACHE_DIR", None)
+if CACHE_DIR is None:
+    CACHE_DIR = "./cache"
+    logger.warn("BOT_CACHE_DIR not found in .env file, using default: './cache'")
