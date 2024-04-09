00b85f2fac305f5394b82f2a53b3f1047470ce9a
HyScript7 <hyscript7@seznam.cz>
2024-04-09 15:27:14 +0200

    AttributeError: Can't pickle local object

--- test/process_executor.py
+++ test/process_executor.py
@@ -5,12 +5,6 @@ import asyncio
 import threading
 from typing import Any, Callable, Coroutine, List, Dict
 
-class ProccessSafeFunction:
-    def __init__(self, func: Callable) -> None:
-        self._func = func
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        self._func(*args, **kwds)
-
 
 class ABCExecutor:
     def __init__(self, coro: Coroutine) -> None:
@@ -18,16 +12,30 @@ class ABCExecutor:
 
     async def wait(self) -> None:
         raise NotImplementedError
-    
+
     def is_set(self) -> bool:
         raise NotImplementedError
 
     def get_result(self) -> Any | None:
         raise NotImplementedError
 
+
 class ProccessExecutor(ABCExecutor):
-    def __init__(self, coro: Coroutine) -> None:
+    def __init__(self, func: Callable) -> None:
+        def _run(
+            val: multiprocessing.managers.ValueProxy, *args: Any, **kwds: Any
+        ) -> Any:
+            val.set(func(*args, **kwds))
+            return val.get()
+
         self._event = multiprocessing.Event()
+        with multiprocessing.Manager() as manager:
+            self._result = manager.Value("i", 0)
+        self._process = multiprocessing.Process(target=_run, args=(self._result, 1, 2, 3))
+        self._process.start()
+
+    def sync(self) -> None:
+        self._process.join()
 
     async def wait(self) -> None:
         await self._event.wait()
@@ -41,3 +49,11 @@ class ProccessExecutor(ABCExecutor):
     def _run(self) -> None:
         self._result = asyncio.run(self._coro())
         self._event.set()
+
+
+def my_func(x: int, y: int, z: int) -> int:
+    return x + y + z
+
+
+if __name__ == "__main__":
+    ProccessExecutor(my_func).sync()
